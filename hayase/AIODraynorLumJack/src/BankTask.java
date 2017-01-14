d05bde4d7a1df505d64827e77af056d46bfe2340
Hayase <midori@riseup.net>
2017-01-13 23:06:35 -0800

    Cleaned up the method that handles when the player is under attack.

--- hayase/AIODraynorLumJack/src/BankTask.java
+++ hayase/AIODraynorLumJack/src/BankTask.java
@@ -54,39 +54,46 @@ public class BankTask extends Task {
         api.execute(webEvent);
 
         /*
-         * People like to lure dark wizards inside Draynor bank--run away from those mages
+         * Survival against dark mages and jail guards
          */
-        if (api.myPlayer().isUnderAttack() && Banks.DRAYNOR.contains(api.myPlayer())) {
-            api.log("Under attack! Running to a safe spot...");
-
-            //Safe spot near abby witch
-            WebWalkEvent runEvent = new WebWalkEvent(new Area(3090, 3263, 3094, 3257));
-            runEvent.setEnergyThreshold(1);
-            api.execute(runEvent);
-
-            //Sleep until we are out of combat
-            new ConditionalSleep(10_000) {
-                @Override
-                public boolean condition() throws InterruptedException {
-                    return !api.myPlayer().isUnderAttack();
-                }
-            }.sleep();
-
-            //Lets hop worlds to get away from the bank mages
-            //apparently only 381++ doesn't bug out?
-            if (api.worlds.hopToF2PWorld()) {
-                api.log("Hopping worlds...");
-            }
-        } else {
+        if (api.myPlayer().isUnderAttack()) {
 
             /*
-             * If we are not in the bank, run as fast as we can to the bank
+             * People like to lure dark wizards inside Draynor bank--run away from those mages
              */
-            api.log("Under attack! Running to the bank");
-            WebWalkEvent runEvent = new WebWalkEvent(Banks.DRAYNOR);
-            runEvent.setEnergyThreshold(1);
-            api.execute(runEvent);
+            if (Banks.DRAYNOR.contains(api.myPlayer())) {
+                api.log("Under attack! Running to a safe spot...");
+
+                //Safe spot near abby witch
+                WebWalkEvent runEvent = new WebWalkEvent(new Area(3090, 3263, 3094, 3257));
+                runEvent.setEnergyThreshold(1);
+                api.execute(runEvent);
+
+                //Sleep until we are out of combat
+                new ConditionalSleep(10_000) {
+                    @Override
+                    public boolean condition() throws InterruptedException {
+                        return !api.myPlayer().isUnderAttack();
+                    }
+                }.sleep();
+
+                //Lets hop worlds to get away from the bank mages
+                if (api.worlds.hopToF2PWorld()) {
+                    api.log("Hopping worlds...");
+                }
+            } else {
+
+                /*
+                 * If we are still under attack and not in the bank, it must be from the jail guard
+                 * Run as fast as we can to the bank
+                 */
+                api.log("Under attack! Running to the bank");
+                WebWalkEvent runEvent = new WebWalkEvent(Banks.DRAYNOR);
+                runEvent.setEnergyThreshold(1);
+                api.execute(runEvent);
+            }
         }
+
 
         RS2Object bankBooth = api.objects.closest(draynorBankRange, "Bank booth");
         NPC banker = api.npcs.closest(draynorBankRange, "Banker");
